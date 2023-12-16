66a4529e99e244e19bebc29fc8a1b455148f5717
david-anav <art_migalev@yahoo.com>
2023-12-16 12:15:28 +0300

    20/63

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,7 +3,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,9 +14,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let count = 0;
+  if(matrix.length>0) {
+      for( let i =0; i< matrix.length; i++) {
+          const arrEl = matrix[i];
+          for (let el = 0; el < arrEl.length; el++){
+              if( arrEl[el] === '^^'){
+                  count++;
+              }
+          }
+      }
+  }
+  return count;
 }
 
 module.exports = {
