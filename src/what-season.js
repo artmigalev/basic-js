296221052974b3d6e0be6c8108a9d45566ef7361
david-anav <art_migalev@yahoo.com>
2023-12-25 12:12:38 +0300

    + whatSeasons

--- src/what-season.js
+++ src/what-season.js
@@ -12,39 +12,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function getSeason(/*date*/) {
-    throw new NotImplementedError('Not implemented');
 
-//   const seasons = ['winter', 'spring', 'summer', "autumn"];
-//   const indSeason = [[11,0,1],[2,3,4],[5,6,7], [8,9,10]];
-//   if(date === Date.now() || date === new Date(undefined) || date === new Date(null)){
-//       throw new Error('Invalid date!')
-//   }
+  const seasons = ['winter', 'spring', 'summer', "autumn"];
+  const indSeason = [[11,0,1],[2,3,4],[5,6,7], [8,9,10]];
+  if(date === Date.now() || date === new Date(undefined) || date === new Date(null)){
+      throw new Error('Invalid date!')
+  }
 
-//   if (arguments.length === 0 ){
-//       return'Unable to determine the time of year!'
-//   }
-//   if (typeof  date === 'string'){
-//       const findWord = (element) => {
-//           const newEl = date;
-//           return element === newEl.toLowerCase();
-//       }
-//           return seasons.find(findWord) === undefined ? 'Invalid date!' : seasons.find(findWord);
-//   }else {
+  if (arguments.length === 0 ){
+      return'Unable to determine the time of year!'
+  }
+  if (typeof  date === 'string'){
+      const findWord = (element) => {
+          const newEl = date;
+          return element === newEl.toLowerCase();
+      }
+          return seasons.find(findWord) === undefined ? 'Invalid date!' : seasons.find(findWord);
+  }else {
 
-//       if ( date.constructor === Date && date.getMonth() <= 11) {
-//       const month = date.getMonth();
-//       for (let i =0 ; i< indSeason.length; i++){
-//           for( let j = 0; j< indSeason[i].length; j++ ){
+      if ( date.constructor === Date && date.getMonth() <= 11) {
+      const month = date.getMonth();
+      for (let i =0 ; i< indSeason.length; i++){
+          for( let j = 0; j< indSeason[i].length; j++ ){
 
-//               if ( indSeason[i][j] === month ){
-//                   return seasons[i];
-//               }
-//           }
-//       }
-//       }else {
-//                throw new Error('Invalid date!')
-//           }
-//   }
+              if ( indSeason[i][j] === month ){
+                  return seasons[i];
+              }
+          }
+      }
+      }else {
+               throw new Error('Invalid date!')
+          }
+  }
 }
 module.exports = {
   getSeason
