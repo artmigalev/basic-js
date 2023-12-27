9c89627729d8a98a1638ea3936319a7397e6a129
david-anav <art_migalev@yahoo.com>
2023-12-27 22:45:55 +0300

    45/33

--- exemple.js
+++ exemple.js
@@ -1,18 +1,46 @@
-function calculateHanoi(disksNumber, turnsSpeed) {
-    let turns = Math.pow(2,disksNumber)-1;
-    let seconds =/* Math.ceil*/ (turns/(turnsSpeed / 3600))/*.toFixed(1))*/
-    // console.log(Math.floor(seconds))
-    let hanoi = new Object;
-    hanoi.turns = turns;
-    hanoi.seconds = Math.floor(seconds);
-    return hanoi;
-}
-function calculateHanoi(disksNumber, turnsSpeed) {
-    let resultHanoi = new Object;
-    resultHanoi.turns = Math.pow(2,disksNumber)-1;
-    resultHanoi.seconds =Math.floor(resultHanoi.turns/(turnsSpeed / 3600))
-    return resultHanoi;
-}
+const chainMaker = {
+    length: 0,
+    arr: [],
+
+    getLength() {
+        return this.length;
+    },
+    addLink(value = '') {
 
 
-console.log(calculateHanoi(5, 4007))
+
+        if(value === ''){
+            this.length++;
+
+            this.arr.push(`(${''})`);
+            return this;
+        }
+        this.arr.push(`( ${value} )`);
+        this.length++;
+        return this;
+
+    },
+    removeLink(position) {
+        if ( position === 0  || position > this.length || position < 0 || typeof position === 'string' ) {
+            this.arr = []
+            throw 'You can\'t remove incorrect link!';
+        }
+        this.arr.splice(position-1,1);
+        this.length--;
+        return this;
+    },
+    reverseChain() {
+        this.arr = this.arr.reverse()
+        return this;
+    },
+    finishChain() {
+        const result = this.arr.join('~~');
+        this.arr = []
+        return result;
+    }
+
+
+};
+
+console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain(),chainMaker.addLink('8.963').reverseChain().reverseChain().reverseChain().reverseChain().addLink({ 0: 'first', 1: 'second', 'length': 2 }).reverseChain().addLink(3.14).addLink('DEF').reverseChain().finishChain())
+console.log(chainMaker.addLink('8.963').reverseChain().reverseChain().reverseChain().reverseChain().addLink({ 0: 'first', 1: 'second', 'length': 2 }).reverseChain().addLink(3.14).addLink('DEF').reverseChain().finishChain())
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,29 +2,51 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    length: 0,
+    arr: [],
+
+    getLength() {
+        return this.length;
+    },
+    addLink(value = '') {
+
+
+
+        if(value === ''){
+            this.length++;
+
+            this.arr.push(`(${''})`);
+            return this;
+        }
+        this.arr.push(`( ${value} )`);
+        this.length++;
+        return this;
+
+    },
+    removeLink(position) {
+        if ( position === 0  || position > this.length || position < 0 || typeof position === 'string' ) {
+            this.arr = []
+            throw 'You can\'t remove incorrect link!';
+        }
+        this.arr.splice(position-1,1);
+        this.length--;
+        return this;
+    },
+    reverseChain() {
+        this.arr = this.arr.reverse()
+        return this;
+    },
+    finishChain() {
+        const result = this.arr.join('~~');
+        this.length = 0;
+        this.arr = [];
+        return result ;
+    }
+
+
 };
 
 module.exports = {
